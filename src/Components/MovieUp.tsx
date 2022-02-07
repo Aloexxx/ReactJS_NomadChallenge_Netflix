eff693fa20cb0d31d02fba7b61e5ad26d694e849
Aloexxx <junbeomb216@gmail.com>
2022-02-08 03:26:01 +0900

    1

--- src/Components/MovieUp.tsx
+++ src/Components/MovieUp.tsx
@@ -115,7 +115,7 @@ const MovieUp = ()=>{
     }
     const toggleLeaving = ()=>setLeaving((prev)=>!prev)
     const onBoxClicked = (movieId:number)=>{
-        history(`/movies/${"up"}/${movieId}`);
+        history(`${process.env.PUBLIC_URL}/movies/${"up"}/${movieId}`);
     }
     useEffect(()=>{
         isLoading?null:console.log(data,"data123")
--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -87,7 +87,7 @@ function Home(){
     const history=useNavigate();
     const onOverlayClick=()=>history(`${process.env.PUBLIC_URL}/`);
     const {scrollY} =useViewportScroll();
-    const bigMovieMatch = useMatch(`/movies/:category/:movieId`);
+    const bigMovieMatch = useMatch(`${process.env.PUBLIC_URL}/movies/:category/:movieId`);
     const [more,setMore] = useState(false);
     const {data:dataNow,isLoading:loadingNow} = useQuery<IGetMoviesResult>(["movies","nowPlaying"],getMoviesNowPlaying)
     const {data:dataLatest,isLoading:loadingLatest} = useQuery<IGetMoviesResult>(["movies","latest"],getMoviesLatest)
